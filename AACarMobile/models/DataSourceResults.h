13ef0e859b3c46128a1be7e190a9352b6e9978c1
Develop <develop@Mac-mini-Sergei.local>
2015-08-13 17:50:27 +0300

    data source, map annotations

---
+++ AACarMobile/models/DataSourceResults.h
@@ -0,0 +1,13 @@
+//
+//  DataSourceResults.h
+//  AACarMobile
+//
+//  Created by Develop on 13.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataSourceResults : NSObject
+
+@end
---
+++ AACarMobile/models/DataSourceResults.m
@@ -0,0 +1,13 @@
+//
+//  DataSourceResults.m
+//  AACarMobile
+//
+//  Created by Develop on 13.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "DataSourceResults.h"
+
+@implementation DataSourceResults
+
+@end
