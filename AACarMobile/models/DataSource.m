84fc8bc73e48545c21455d3e6ea7409d665261f7
Develop <develop@Mac-mini-Sergei.local>
2015-08-14 17:59:14 +0300

    data source & paging

---
+++ AACarMobile/models/DataSource.m
@@ -0,0 +1,13 @@
+//
+//  DataSource.m
+//  AACarMobile
+//
+//  Created by Develop on 14.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "DataSource.h"
+
+@implementation DataSource
+
+@end
---
+++ AACarMobile/models/DataSourceMatch.m
@@ -0,0 +1,13 @@
+//
+//  DataSourceMatch.m
+//  AACarMobile
+//
+//  Created by Develop on 14.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "DataSourceMatch.h"
+
+@implementation DataSourceMatch
+
+@end
