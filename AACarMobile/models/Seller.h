9a1b8d1fad5225cb00c23ed8281087f1b4735fed
Develop <develop@Mac-mini-Sergei.local>
2015-08-17 18:12:15 +0300

    all function, begin timer

---
+++ AACarMobile/models/Seller.h
@@ -0,0 +1,13 @@
+//
+//  Seller.h
+//  AACarMobile
+//
+//  Created by Develop on 17.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Seller : NSObject
+
+@end
---
+++ AACarMobile/models/Seller.m
@@ -0,0 +1,13 @@
+//
+//  Seller.m
+//  AACarMobile
+//
+//  Created by Develop on 17.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "Seller.h"
+
+@implementation Seller
+
+@end
