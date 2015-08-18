f467d670599c7ce664683a696afb9caf97600e7e
Develop <develop@Mac-mini-Sergei.local>
2015-08-18 18:10:07 +0300

    orders

---
+++ AACarMobile/controllers/OrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderViewController.h
+//  AACarMobile
+//
+//  Created by Develop on 18.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OrderViewController : NSViewController
+
+@end
---
+++ AACarMobile/controllers/OrderViewController.m
@@ -0,0 +1,22 @@
+//
+//  OrderViewController.m
+//  AACarMobile
+//
+//  Created by Develop on 18.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "OrderViewController.h"
+
+@interface OrderViewController ()
+
+@end
+
+@implementation OrderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
