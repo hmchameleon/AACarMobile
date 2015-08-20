c88f2a2d06507771b770e7ae2f6251bfad7e282d
Develop <develop@Mac-mini-Sergei.local>
2015-08-20 17:38:56 +0300

    remake order, send singleton

---
+++ AACarMobile/models/SendManager.h
@@ -0,0 +1,13 @@
+//
+//  SendManager.h
+//  AACarMobile
+//
+//  Created by Develop on 20.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SendManager : NSObject
+
+@end
---
+++ AACarMobile/models/SendManager.m
@@ -0,0 +1,13 @@
+//
+//  SendManager.m
+//  AACarMobile
+//
+//  Created by Develop on 20.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "SendManager.h"
+
+@implementation SendManager
+
+@end
