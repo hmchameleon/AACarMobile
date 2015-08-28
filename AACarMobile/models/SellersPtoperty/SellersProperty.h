c3887b23e25991fcf47db15111c25dfb1d424efc
Develop <develop@Mac-mini-Sergei.local>
2015-08-28 17:27:09 +0300

    first alpha done

---
+++ AACarMobile/models/SellersPtoperty/SellersProperty.h
@@ -0,0 +1,13 @@
+//
+//  SellersProperty.h
+//  AACarMobile
+//
+//  Created by Develop on 28.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SellersProperty : NSObject
+
+@end
---
+++ AACarMobile/models/SellersPtoperty/SellersProperty.m
@@ -0,0 +1,13 @@
+//
+//  SellersProperty.m
+//  AACarMobile
+//
+//  Created by Develop on 28.08.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import "SellersProperty.h"
+
+@implementation SellersProperty
+
+@end
